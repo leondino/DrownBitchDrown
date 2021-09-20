4487690a064d37c2c1689ee5edf82b5fadf45757
Aslesh <cjullienmallet@gmail.com>
2021-09-20 14:59:08 +0200

    Add: Possibility to change color towers

--- DrownBitchDrown/Assets/Scripts/TowerColor.cs
+++ DrownBitchDrown/Assets/Scripts/TowerColor.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class TowerColor : MonoBehaviour
 {
-    GameObject[] towers;
+    List<Transform> towers= new List<Transform>();
 
-    public void ChangeTower(string tag)
+    private void Awake()
     {
+        foreach (Transform child in transform)
+        {
+            towers.Add(child);
+        }
+    }
 
+    public void ChangeTower(string tag, int section)
+    {
+        switch (tag)
+        {
+            case "rood":
+                ChangeColor(section, new Color(1, 0, 0));
+                break;
+
+            case "geel":
+                ChangeColor(section, new Color(1, 1, 0));
+                break;
+
+            case "groen":
+                ChangeColor(section, new Color(0, 1, 0));
+                break;
+        }
+    }
+
+    private void ChangeColor(int section, Color color)
+    {
+        towers[section].GetComponent<MeshRenderer>().material.color = color;
     }
 }
