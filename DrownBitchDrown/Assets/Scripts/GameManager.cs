1284e8caee8c580665615ff68bf4555eeb5f684b
Léon Imanuel <leondino0309@gmail.com>
2021-09-21 15:23:16 +0200

    Game now starts of in low tide and switched automaticly to high tide after some time. Also able to switch back to low tide when method is called where it should be.

--- DrownBitchDrown/Assets/Scripts/GameManager.cs
+++ DrownBitchDrown/Assets/Scripts/GameManager.cs
@@ -6,26 +6,47 @@ public class GameManager : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject startPosition;
+
+    public static GameManager instance;
+
+    [HideInInspector]
     public bool isHighTide;
+
+    private float tideTimer = 0;
+    private int lowTideDuration = 5;
+    private int highTideDuration = 7;
+
     // Start is called before the first frame update
     void Start()
     {
         isHighTide = false;
+        instance = this;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        tideTimer += Time.deltaTime;
         // All code for high tide gameplay
         if (isHighTide)
         {
-
+            // High tide timer
+            if (tideTimer >= highTideDuration)
+            {
+                tideTimer = 0;
+                // Check if player in correct field then switch tides.
+            }
         }
 
         // All code for low tide gameplay
         else
         {
-
+            // Low tide timer
+            if (tideTimer >= lowTideDuration)
+            {
+                tideTimer = 0;
+                SwitchTide();
+            }
         }
     }
 
